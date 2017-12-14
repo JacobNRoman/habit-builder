5d6926f67c48549371507f1c6024e137141ce22e
Jacob Roman <jnroman7@gmail.com>
2017-12-13 21:21:11 -0600

    initial commit

---
+++ src/main/java/org/launchcode/habitbuilder/HabitBuilderApplication.java
@@ -0,0 +1,12 @@
+package org.launchcode.habitbuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabitBuilderApplication.class, args);
+	}
+}
